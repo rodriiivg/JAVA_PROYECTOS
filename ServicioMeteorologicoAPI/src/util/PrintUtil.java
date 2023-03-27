b423dd918c323892dec6596f24cf52a0216efe19
Rodrigo Valdés García <115140411+rodriiivg@users.noreply.github.com>
2023-03-27 13:59:30 +0200

    Add files via upload

---
+++ ServicioMeteorologicoAPI/src/util/PrintUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.util.Map;
+import java.util.Optional;
+import model.TemperaturaHora;
+
+/**
+ *
+ * @author rodrigo valdes
+ */
+public class PrintUtil {
+public static void printMapProvinciaTemperaturaHora(Map<String, Optional<TemperaturaHora>> map) {
+
+	
+		map.keySet().stream().sorted().forEach(provincia -> {
+			TemperaturaHora t = map.get(provincia).get();
+			System.out.printf("Provincia: %-25s | Temperatura: % 6.2f°C | Hora: %s%n", provincia, t.getTemperatura(),
+					t.getHora().toString());
+		});
+		System.out.println("");
+	}
+}
